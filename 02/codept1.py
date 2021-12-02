48a2e0d6b3433d5bc39168d0ce9341b3a4e764f1
CTAKnoll <ctk140030@utdallas.edu>
2021-12-01 21:14:42 -0800

    Day 2

---
+++ 02/codept1.py
@@ -0,0 +1,19 @@
+coord = [0,0]
+
+def parse(str):
+    arr = str.split(" ")
+    if(arr[0] == "forward"):
+        coord[0] = coord[0] + int(arr[1])
+    if(arr[0] == "up"):
+        coord[1] = coord[1] - int(arr[1])
+    if(arr[0] == "down"):
+        coord[1] = coord[1] + int(arr[1])
+
+with open('input.txt') as f:
+    content = f.read()
+    for str in content.split("\n"):
+        print(str)
+        parse(str)
+print(coord[0] * coord[1])
+
+
